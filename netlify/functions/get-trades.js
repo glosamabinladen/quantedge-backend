c856f1c494edf7b263b01e682ce5a7674e43a4ce
glosamabinladen <glosamabinladen@gmail.com>
2026-05-11 17:51:58 -0700

    Update get-trades.js

--- netlify/functions/get-trades.js
+++ netlify/functions/get-trades.js
@@ -6,7 +6,7 @@ import { getStore } from "@netlify/blobs";
 
 const CORS = {
   "Content-Type":                "application/json",
-  "Access-Control-Allow-Origin": "https://glosamabinladen.github.io",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods":"GET, OPTIONS",
   "Access-Control-Allow-Headers":"Content-Type",
 };
